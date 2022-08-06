6c2287329f4ead2f847c24fc567e0e7f634638b5
buyanne <1903683577@qq.com>
2022-08-06 19:06:16 +0800

    drawjs的一些修改

--- JavaScript/draw.js
+++ JavaScript/draw.js
@@ -1,41 +1,59 @@
-
-
 //思考一下画板怎么写
+//好的宝，但是现在有一个问题就是back
 
+//位置数组
+var position = [0];
+//判断鼠标是否按下
+var mousePressed = false;
+var mouseIsInCanvas = true;
 var canvas = document.getElementById("Canvas");
 
 //画笔
 var ctx = canvas.getContext("2d");
+
+
 ctx.fillStyle = "black";
 ctx.strokeStyle = "none";
 ctx.lineWidth = 3;
-//位置数组
 
-var position = [0];
-
-canvas.onmousedown = function (e) {
-    //当鼠标按下的时候flag为true，然后记录位置
-    flag = true;
+//判断鼠标是否按下
+document.onmousedown = (e) => {
+    mousePressed = true;
     position[0] = e.clientX;
     position[1] = e.clientY;
-};
+}
+
+//判断鼠标是否松开
+document.onmouseup = (e) => {
+    mousePressed = false;
+}
+
+
+//画线的函数
+function draw(startX, startY, endX, endY) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+
 canvas.onmousemove = function (e) {
-    if (flag) {
-    //画线，然后记录位置
+    if (mousePressed && mouseIsInCanvas) {
+        //画线，然后记录位置
         draw(position[0], position[1], e.clientX, e.clientY);
         position[0] = e.clientX;
         position[1] = e.clientY;
     }
 };
-canvas.onmouseup = function () {
-    flag = false;
-};
-
-//画线的函数
-function draw(starX, starY, endX, endY) {
-    ctx.beginPath();
-    ctx.moveTo(starX, starY);
-    ctx.lineTo(endX, endY);
-    ctx.closePath();
-    ctx.stroke();
+//在鼠标离开canvas时
+canvas.onmouseleave = (e) => {
+    mouseIsInCanvas = false;
+}
+//鼠标进入canvas时
+canvas.onmouseenter = (e) => {
+    mouseIsInCanvas = true;
+    position[0] = e.clientX;
+    position[1] = e.clientY;
 }
