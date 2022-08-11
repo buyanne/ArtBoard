08f21277736bf90725ed86522984ca86a4c20943
WuDu <2084707851@qq.com>
2022-08-11 18:37:14 +0800

    实现了按下按钮后变色以及网页状态变换

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -34,3 +34,42 @@ function windowAddMouseWheel() {
     window.onmousewheel = document.onmousewheel = scrollFunc;
 }
 
+
+
+/* 
+状态获取函数，通过网页上按钮的单击，判断当前的状态
+一部分按下即使用的功能就不列状态了
+状态列表：0 color，1 size，2 pencil，3 line,4 circle,5square,6 area, 8 rubber
+默认状态：2（笔）
+ */
+
+var buttons = document.querySelectorAll(".ToolsDiv input")
+//基本状态
+var MainArtBoardDiv = document.querySelector(".MainArtBoardDiv")
+buttons[2].className="after";
+//console.log(buttons)
+
+
+//绑定事件
+for(let i=0;i<=8;i++){
+    if(i !== 7){
+        buttons[i].addEventListener("click",function changeState(){
+            //点击事件
+            
+            //改变当前网页的state
+            MainArtBoardDiv.boardState = i;
+            //console.log(MainArtBoardDiv.boardState);
+            
+            //改变css样式
+            buttons[i].className="after";
+            for(let j = 0;j<=8;j++){
+                if(j!==7&&j!==i){
+                    buttons[j].className="before";
+                }
+            }
+        });
+    }
+    
+}
+
+
